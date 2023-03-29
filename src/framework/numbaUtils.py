a0edd69e08b58c2f8d3aac68c22d5b6ccad849c7
Austin Polk <austinpolk32@gmail.com>
2023-03-29 13:34:18 -0500

    Changed kernel indexing to use single threadIdx, can now correctly extract 5 indices from this. Decided not to use flattened versions of res and feature anymore, as it makes more sense to just let Python handle the multidimensional indexing. The first complete run of the project has occurred, so now I need to optimize this further, as it's training time wasn't as good as I expected.

--- src/framework/numbaUtils.py
+++ src/framework/numbaUtils.py
@@ -1,4 +1,5 @@
 from numba import cuda
+import numpy as np
 import math
 
 '''
@@ -30,58 +31,49 @@ import math
 
 '''
 
-def launchGPUResKernel(flatFeature, featureShape, flatRes, resShape, dilate):
+def launchGPUResKernel(feature, res, dilate):
     
-    global fShape, rShape, threadDimensions
+    global totalThreads
 
-    fShape = featureShape
-    rShape = resShape
+    fShape = feature.shape
     
-    threadDimensions = (fShape[0] - 2 * dilate, fShape[1] - 2 * dilate, fShape[2] * 9 * fShape[3])
+    ## setup for kernel execution
+    threadDimensions = np.array([(fShape[0] - 2 * dilate), (fShape[1] - 2 * dilate), fShape[2], 9, fShape[3]])
+    totalThreads = threadDimensions.prod()
+    threadsPerBlock = (128)
+    blocksPerGrid = math.ceil(totalThreads / threadsPerBlock)
+
+    ## transfer data to device
+    d_feature = cuda.to_device(np.ascontiguousarray(feature))
+    d_res = cuda.to_device(np.ascontiguousarray(res))
+    d_threadDimensions = cuda.to_device(np.ascontiguousarray(threadDimensions))
+
+    ## run GPU kernel
+    GPUResKernel[blocksPerGrid, threadsPerBlock](d_feature, d_res, dilate, fShape[3], d_threadDimensions)
+
+    ## transfer result back
+    d_res.copy_to_host(res)
     
-    threadsPerBlock = (6, 6, 6)
-    blocksPerGrid_x = math.ceil(threadDimensions[0] / threadsPerBlock[0])
-    blocksPerGrid_y = math.ceil(threadDimensions[1] / threadsPerBlock[1])
-    blocksPerGrid_z = math.ceil(threadDimensions[2] / threadsPerBlock[2])
-    blocksPerGrid = (blocksPerGrid_x, blocksPerGrid_y, blocksPerGrid_z)
-
-    d_feature = cuda.to_device(flatFeature)
-    d_res = cuda.to_device(flatRes)
-
-    GPUResKernel[blocksPerGrid, threadsPerBlock](d_feature, d_res, dilate)
-
-    flatRes = d_res.copy_to_host()
-
-    res = flatRes.reshape(rShape)
-
-   
     return res
 
 @cuda.jit
-def GPUResKernel(d_feature, d_res, dilate):
-    i, j, m = cuda.grid(3)
+def GPUResKernel(d_feature, d_res, dilate, f3, d_threadDimensions):
+    threadIdx = cuda.grid(1)
 
-    if i < threadDimensions[0] and j < threadDimensions[1] and m < threadDimensions[2]:
-        a, b, k = getABK(m)
-        tmpf = AccessFeature(d_feature, i + (b//3) * dilate, j + (b%3) * dilate, a, k)
-        AssignRes(d_res, i, j, a, fShape[3] * b + k, tmpf)
+    if threadIdx < totalThreads:
+        i, j, a, b, k = getIJABK(threadIdx, d_threadDimensions)
+        d_res[i, j, a, f3 * b + k] = d_feature[i + (b//3) * dilate, j + (b%3) * dilate, a, k]
 
 ### get loop index parameters
 @cuda.jit(device=True)
-def getABK(m):
-    a = m // (9 * fShape[3])
-    m -= a * 9 * fShape[3]
-    b = m // fShape[3]
-    m -= b * fShape[3]
+def getIJABK(m, threadDimensions):
+    i = m // (threadDimensions[1] * threadDimensions[2] * threadDimensions[3] * threadDimensions[4])
+    m -= i * (threadDimensions[1] * threadDimensions[2] * threadDimensions[3] * threadDimensions[4])
+    j = m // (threadDimensions[2] * threadDimensions[3] * threadDimensions[4])
+    m -= j * (threadDimensions[2] * threadDimensions[3] * threadDimensions[4])
+    a = m // (threadDimensions[3] * threadDimensions[4])
+    m -= a * (threadDimensions[3] * threadDimensions[4])
+    b = m // (threadDimensions[4])
+    m -= b * (threadDimensions[4])
     k = m
-    return a, b, k
-
-### access the flattened feature array in 4D
-@cuda.jit(device=True)
-def AccessFeature(arr, w, x, y, z):
-    return arr[fShape[3] * fShape[2] * fShape[1] * w + fShape[3] * fShape[2] * x + fShape[3] * y + z]
-
-### access the flattened res array in 4D
-@cuda.jit(device=True)
-def AssignRes(arr, w, x, y, z, value):
-    arr[rShape[3] * rShape[2] * rShape[1] * w + rShape[3] * rShape[2] * x + rShape[3] * y + z] = value
+    return i, j, a, b, k
