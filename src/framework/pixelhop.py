32897aa4ed49c8bcc2ed372bfc8422b3deed4431
Austin Polk <austinpolk32@gmail.com>
2023-03-30 14:21:36 -0500

    Move GPU Kernel to pixelhop.py, add and implement a Bias kernel as part of moving pixelhop_fit to the GPU. To save time and not have to transfer data as much, the entire pixelhop_unit has been compressed into one function in pixelhop_unit_gpu. Data is mostly kept on the device unless executing the saab or copying over the final result (right now, the final result is computed with cpu, but this is the end goal).

--- src/framework/pixelhop.py
+++ src/framework/pixelhop.py
@@ -10,10 +10,11 @@
 
 # return <4-D array>, (N, H_new, W_new, D_new)
 
+import math
 import numpy as np 
 import pickle
 import time
-
+from numba import cuda
 from framework.saab import Saab
 from framework.numbaUtils import launchGPUResKernel
 
@@ -51,8 +52,8 @@ def Pixelhop_fit(weight_path, feature, useDC):
     bias = pca_params['Layer_%d/bias' % 0]
     # Add bias
     feature_w_bias = feature + 1 / np.sqrt(feature.shape[3]) * bias
-    print("bias value: %s"%str(bias))
-    print("feature with bias shape: %s"%str(feature_w_bias.shape))
+    print("       <Info>        bias value: %s"%str(bias))
+    print("       <Info>        feature with bias shape: %s"%str(feature_w_bias.shape))
     # Transform to get data for the next stage
     transformed_feature = np.matmul(feature_w_bias, np.transpose(weight))
     if useDC == True:
@@ -64,16 +65,106 @@ def Pixelhop_fit(weight_path, feature, useDC):
     return transformed_feature
 
 def PixelHop_Unit(feature, dilate=1, pad='reflect', weight_name='tmp.pkl', getK=False, useDC=False, energypercent=0.92):
-    print("=========== Start: PixelHop_Unit")
-    t0 = time.time()
-    feature = PixelHop_8_Neighbour(feature, dilate, pad)
-    if getK == True:
-        saab = Saab('../weight/'+weight_name, kernel_sizes=np.array([2]), useDC=useDC, energy_percent=energypercent)
-        saab.fit(feature)
-    transformed_feature = Pixelhop_fit('../weight/'+weight_name, feature, useDC) 
-    print("       <Info>        Output feature shape: %s"%str(transformed_feature.shape))
-    print("=========== End: PixelHop_Unit -> using %10f seconds"%(time.time()-t0))
+    transformed_feature = PixelHop_Unit_GPU(feature, dilate, pad, weight_name, getK, useDC, energypercent)
+    #print("=========== Start: PixelHop_Unit")
+    #t0 = time.time()
+    #feature = PixelHop_8_Neighbour(feature, dilate, pad)
+    #if getK == True:
+    #    saab = Saab('../weight/'+weight_name, kernel_sizes=np.array([2]), useDC=useDC, energy_percent=energypercent)
+    #    saab.fit(feature)
+    #transformed_feature = Pixelhop_fit('../weight/'+weight_name, feature, useDC) 
+    #print("       <Info>        Output feature shape: %s"%str(transformed_feature.shape))
+    #print("=========== End: PixelHop_Unit -> using %10f seconds"%(time.time()-t0))
     return transformed_feature
 
+def PixelHop_Unit_GPU(feature, dilate=1, pad='reflect', weight_name='tmp.pkl', getK=False, useDC=False, energypercent=0.92):
+    print("=========== Start: PixelHop_Unit_GPU")
+    t0 = time.time()
+    weight_path = '../weight/'+weight_name  ## weight path for saab
+    ### NEIGHBOUR/BIAS PADDING ###
+    S = feature.shape
+    if pad == 'reflect':
+        feature = np.pad(feature, ((0,0),(dilate, dilate),(dilate, dilate),(0,0)), 'reflect')
+    elif pad == 'zeros':
+        feature = np.pad(feature, ((0,0),(dilate, dilate),(dilate, dilate),(0,0)), 'constant', constant_values=0)
 
+    if pad == "none":
+        resShape = (S[0], (S[2]-2*dilate), (S[1]-2*dilate), 9*S[3])
+    else:
+        resShape = (S[0], S[2], S[1], 9*S[3])
+    fShape = feature.shape
+    ### NEIGHBOUR/BIAS THREAD SETUP###
+    threadDimensions = np.array([(fShape[2] - 2 * dilate), (fShape[1] - 2 * dilate), fShape[0], 9, fShape[3]])
+    d_threadDimensions = cuda.to_device(np.ascontiguousarray(threadDimensions))
+    totalThreads = threadDimensions.prod()
+    threadsPerBlock = (64)
+    blocksPerGrid = math.ceil(totalThreads / threadsPerBlock)
+    ### TRANSFER DATA TO DEVICE ###
+    d_feature = cuda.to_device(np.ascontiguousarray(feature))
+    d_res = cuda.device_array(resShape)
+    ### INVOKE NEIGHBOR KERNEL IF USING SAAB ###
+    if getK == True:
+        GPU_8_Neighbour[blocksPerGrid, threadsPerBlock](d_feature, d_res, dilate, fShape[3], d_threadDimensions)
+        resNeighbour = d_res.copy_to_host()
+        saab = Saab(weight_path, kernel_sizes=np.array([2]), useDC=useDC, energy_percent=energypercent)
+        saab.fit(resNeighbour)
+    ### GET BIAS AND WEIGHT ###
+    fr = open(weight_path, 'rb')
+    pca_params = pickle.load(fr)                
+    fr.close()
+    weight = pca_params['Layer_0/kernel'].astype(np.float32)
+    bias = pca_params['Layer_%d/bias' % 0]
+    ### INVOKE BIAS KERNEL ###
+    GPU_Feature_Bias[blocksPerGrid, threadsPerBlock](d_feature, d_res, dilate, fShape[3], bias, d_threadDimensions)
+    ### INVOKE MATRIX MULT KERNEL ###
+    #d_weight = cuda.to_device(np.ascontiguousarray(np.transpose(weight)))
+    # currently is not ported to GPU yet
+    feature_w_bias = d_res.copy_to_host()
+    transformed_feature = np.matmul(feature_w_bias, np.transpose(weight))
+    ### INVOKE DC KERNEL IF USING DC ###
+    # currently is not ported to GPU yet
+    if useDC == True:
+        e = np.zeros((1, weight.shape[0]))
+        e[0, 0] = 1
+        transformed_feature -= bias * e
+    ### COPY FINAL RESULT TO HOST ###
+    result = transformed_feature    # technically already on host in current implementation
+    print("       <Info>        Output feature shape: %s"%str(result.shape))
+    print("=========== End: PixelHop_Unit_GPU -> using %10f seconds"%(time.time()-t0))
+    return result
 
+@cuda.jit
+def GPU_DC():
+    pass
+
+@cuda.jit
+def GPU_MatMul():
+    pass
+
+@cuda.jit
+def GPU_8_Neighbour(d_feature, d_res, dilate, f3, d_threadDimensions):
+    threadIdx = cuda.grid(1)
+    i, j, a, b, k = indices(threadIdx, d_threadDimensions)
+    if i < d_threadDimensions[0]:
+        d_res[a, j, i, f3 * b + k] = d_feature[a, j + (b%3) * dilate, i + (b//3) * dilate, k]
+
+@cuda.jit
+def GPU_Feature_Bias(d_feature, d_res, dilate, f3, bias, d_threadDimensions):
+    threadIdx = cuda.grid(1)
+    i, j, a, b, k = indices(threadIdx, d_threadDimensions)
+    if i < d_threadDimensions[0]:
+        d_res[a, j, i, f3 * b + k] = d_feature[a, j + (b%3) * dilate, i + (b//3) * dilate, k] + 1 / math.sqrt(f3) * bias
+
+### get loop index parameters for neighbour/bias kernel
+@cuda.jit(device=True)
+def indices(m, threadDimensions):
+    i = m // (threadDimensions[1] * threadDimensions[2] * threadDimensions[3] * threadDimensions[4])
+    m -= i * (threadDimensions[1] * threadDimensions[2] * threadDimensions[3] * threadDimensions[4])
+    j = m // (threadDimensions[2] * threadDimensions[3] * threadDimensions[4])
+    m -= j * (threadDimensions[2] * threadDimensions[3] * threadDimensions[4])
+    a = m // (threadDimensions[3] * threadDimensions[4])
+    m -= a * (threadDimensions[3] * threadDimensions[4])
+    b = m // (threadDimensions[4])
+    m -= b * (threadDimensions[4])
+    k = m
+    return i, j, a, b, k
