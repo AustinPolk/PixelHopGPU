233d2e2e979e011e41fc2cf33b18354d99486fc5
Austin Polk <austinpolk32@gmail.com>
2023-03-31 18:27:30 -0500

    Remove numba utils, remove CPU code from pixelhop.py

--- src/framework/pixelhop.py
+++ src/framework/pixelhop.py
@@ -16,66 +16,6 @@ import pickle
 import time
 from numba import cuda
 from framework.saab import Saab
-from framework.numbaUtils import launchGPUResKernel
-
-def PixelHop_8_Neighbour(feature, dilate, pad):
-    print("------------------- Start: PixelHop_8_Neighbour")
-    print("       <Info>        Input feature shape: %s"%str(feature.shape))
-    print("       <Info>        dilate: %s"%str(dilate))
-    print("       <Info>        padding: %s"%str(pad))
-    t0 = time.time()
-    S = feature.shape
-    if pad == 'reflect':
-        feature = np.pad(feature, ((0,0),(dilate, dilate),(dilate, dilate),(0,0)), 'reflect')
-    elif pad == 'zeros':
-        feature = np.pad(feature, ((0,0),(dilate, dilate),(dilate, dilate),(0,0)), 'constant', constant_values=0)
-
-    if pad == "none":
-        resShape = (S[0], (S[2]-2*dilate), (S[1]-2*dilate), 9*S[3])
-    else:
-        resShape = (S[0], S[2], S[1], 9*S[3])
-        
-    res = launchGPUResKernel(feature, resShape, dilate)
-    
-    print("       <Info>        Output feature shape: %s"%str(res.shape))
-    print("------------------- End: PixelHop_8_Neighbour -> using %10f seconds"%(time.time()-t0))
-    return res 
-
-def Pixelhop_fit(weight_path, feature, useDC):
-    print("------------------- Start: Pixelhop_fit")
-    print("       <Info>        Using weight: %s"%str(weight_path))
-    t0 = time.time()
-    fr = open(weight_path, 'rb')
-    pca_params = pickle.load(fr)                
-    fr.close()
-    weight = pca_params['Layer_0/kernel'].astype(np.float32)
-    bias = pca_params['Layer_%d/bias' % 0]
-    # Add bias
-    feature_w_bias = feature + 1 / np.sqrt(feature.shape[3]) * bias
-    print("       <Info>        bias value: %s"%str(bias))
-    print("       <Info>        feature with bias shape: %s"%str(feature_w_bias.shape))
-    # Transform to get data for the next stage
-    transformed_feature = np.matmul(feature_w_bias, np.transpose(weight))
-    if useDC == True:
-        e = np.zeros((1, weight.shape[0]))
-        e[0, 0] = 1
-        transformed_feature -= bias * e
-    print("       <Info>        Transformed feature shape: %s"%str(transformed_feature.shape))
-    print("------------------- End: Pixelhop_fit -> using %10f seconds"%(time.time()-t0))
-    return transformed_feature
-
-def PixelHop_Unit(feature, dilate=1, pad='reflect', weight_name='tmp.pkl', getK=False, useDC=False, energypercent=0.92):
-    transformed_feature = PixelHop_Unit_GPU(feature, dilate, pad, weight_name, getK, useDC, energypercent)
-    #print("=========== Start: PixelHop_Unit")
-    #t0 = time.time()
-    #feature = PixelHop_8_Neighbour(feature, dilate, pad)
-    #if getK == True:
-    #    saab = Saab('../weight/'+weight_name, kernel_sizes=np.array([2]), useDC=useDC, energy_percent=energypercent)
-    #    saab.fit(feature)
-    #transformed_feature = Pixelhop_fit('../weight/'+weight_name, feature, useDC) 
-    #print("       <Info>        Output feature shape: %s"%str(transformed_feature.shape))
-    #print("=========== End: PixelHop_Unit -> using %10f seconds"%(time.time()-t0))
-    return transformed_feature
 
 def PixelHop_Unit_GPU(feature, dilate=1, pad='reflect', weight_name='tmp.pkl', getK=False, useDC=False, energypercent=0.92):
     print("=========== Start: PixelHop_Unit_GPU")
@@ -97,7 +37,7 @@ def PixelHop_Unit_GPU(feature, dilate=1, pad='reflect', weight_name='tmp.pkl', g
         resShape = (S[0], S[2], S[1], 9*S[3])
     fShape = feature.shape
 
-    ### NEIGHBOUR/BIAS THREAD SETUP###
+    ### NEIGHBOUR/BIAS KERNEL THREAD SETUP###
     threadDimensions = np.array([(fShape[2] - 2 * dilate), (fShape[1] - 2 * dilate), fShape[0], 9, fShape[3]])
     d_threadDimensions = cuda.to_device(np.ascontiguousarray(threadDimensions))
     totalThreads = threadDimensions.prod()
